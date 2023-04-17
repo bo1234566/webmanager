b24678f392237525533da852bdb9727bf73a5dbd
bo1234566 <wangbohpu@163.com>
2023-04-17 21:22:55 +0800

    base function of sql8 and mvn_config

--- src/main/java/com/nowcoder/service/QiniuService.java
+++ src/main/java/com/nowcoder/service/QiniuService.java
@@ -28,7 +28,7 @@ public class QiniuService {
 
     private static String QINIU_IMAGE_DOMAIN_HuaDong_ZheJiang = "http://rrpu1mv38.bkt.clouddn.com/";
 
-    private static String QINIU_IMAGE_DOMAIN_HuaBei = "http://rrpi24ash.hb-bkt.clouddn.com/";
+    private static String QINIU_IMAGE_DOMAIN_HuaBei = "http://img.webcloud123.top/";
 
     //构造一个带指定 Region 对象的配置类
 //    Configuration cfg = new Configuration(Region.huadongZheJiang2());
